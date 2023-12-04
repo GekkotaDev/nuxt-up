46656ee98f1e4b2f9ae10db4b2da89113919fe57
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2023-12-04 20:55:44 +0800

    Added eslint-plugin-unicorn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -31,7 +31,13 @@ module.exports = {
     parser: "@typescript-eslint/parser",
     sourceType: "module",
   },
-  plugins: ["@typescript-eslint", "vue", "simple-import-sort", "pii"],
+  plugins: [
+    "@typescript-eslint",
+    "vue",
+    "simple-import-sort",
+    "pii",
+    "unicorn",
+  ],
   rules: {
     "simple-import-sort/imports": "warn",
     "simple-import-sort/exports": "warn",
@@ -40,5 +46,36 @@ module.exports = {
     "pii/no-email": "error",
     "pii/no-ip": "error",
     "pii/no-dob": "warn",
+
+    /*
+     ?  Unicorn autofixable subset.
+     ?
+     ?  Only a subset of eslint-plugin-unicorn's rules were enabled within this
+     ?  configuration. It's highly opinionated nature may prove to be too strict
+     ?  for use, and even then only auto-fixable rules were enabled to get it
+     ?  out of the user's way.
+     */
+    "no-lonely-if": "warn",
+
+    "unicorn/better-regex": "warn",
+    "unicorn/catch-error-name": ["warn", {}],
+    "unicorn/explicit-length-check": "warn",
+    "unicorn/new-for-builtins": "warn",
+    "unicorn/no-for-loop": "warn",
+    "unicorn/no-instanceof-array": "warn",
+    "unicorn/no-lonely-if": "warn",
+    "unicorn/no-new-array": "warn",
+    "unicorn/no-typeof-undefined": "warn",
+    "unicorn/no-useless-length-check": "warn",
+    "unicorn/no-useless-spread": "warn",
+    "unicorn/numeric-separators-style": "warn",
+    "unicorn/prefer-array-flat": "warn",
+    "unicorn/prefer-array-flat-map": "warn",
+    "unicorn/prefer-date-now": "warn",
+    "unicorn/prefer-keyboard-event-key": "warn",
+    "unicorn/prefer-modern-math-apis": "warn",
+    "unicorn/prefer-node-protocol": "warn",
+    "unicorn/prefer-set-size": "warn",
+    "unicorn/require-array-join-separator": "warn",
   },
 };
