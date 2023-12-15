315cb8e7b0fdd8d6a59f0117b108ce745c695621
gekkotadev <114091010+GekkotaDev@users.noreply.github.com>
2023-12-15 23:48:56 +0800

    Removed Nuxt UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
     "@nuxt/content",
-    "@nuxt/ui",
     "@nuxtjs/color-mode",
     "@unocss/nuxt",
     "@vueuse/nuxt",
